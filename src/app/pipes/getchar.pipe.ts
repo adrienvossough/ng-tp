628f89ad9911f39aa6b75fdfd5e1f3184acc531a
Enzo <e.radnai@hotmail.fr>
2020-09-16 16:03:32 +0200

    edit custom pipe

--- src/app/pipes/getchar.pipe.ts
+++ src/app/pipes/getchar.pipe.ts
@@ -5,8 +5,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class GetCharPipe implements PipeTransform {
 
-  transform(text: string): unknown {
-    return text[0];
+  transform(text: string, pos?: number): string {
+    if (pos && text.length >= pos) { // on teste le paramètre 'pos'
+      return text[pos]; // retourne le caractère en position choisie
+    }
+    return text[0]; // retourne le premier caractère d'une chaîne.
   }
 
 }
