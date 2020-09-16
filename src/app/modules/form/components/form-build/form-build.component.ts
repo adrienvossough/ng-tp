ad61b5f4ce20be8d6bb716be227056e8fff7434d
Thomas.D <thomlgt@hotmail.fr>
2020-09-16 14:46:42 +0200

    form builder Ok

--- src/app/modules/form/components/form-build/form-build.component.ts
+++ src/app/modules/form/components/form-build/form-build.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+//Import de FormBuilder
+import { FormBuilder } from '@angular/forms';
+//Import de Validators pour les formulaires
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-build',
@@ -7,9 +11,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormBuildComponent implements OnInit {
 
-  constructor() { }
+  //FormBuilder en paramètre du constructeur
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+  }
+
+  //On met en place un formGroup qui permet de définir des paramètres au formulaire
+  userProfileForm = this.fb.group({
+
+    //Les paramètres nom et prénom sont requis
+    nom : ['', Validators.required],
+    prenom : ['', Validators.required],
+    //On créer un "sous" FormGroup dans un FormGroup qui permet de lier des paramètres entre eux
+    adresse : this.fb.group({
+      numero : [''],
+      rue : [''],
+      cp : ['']
+    })
+  });
+
+  onSubmit() {
+    console.log(this.userProfileForm.value);
+
+    //var nom = this.userProfilForm.get('nom').value
   }
 
 }
