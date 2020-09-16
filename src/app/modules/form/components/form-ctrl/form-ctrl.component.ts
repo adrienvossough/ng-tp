3297f9fe48981d5faeac1b6c2e234a18a88c9b01
Thomas.D <thomlgt@hotmail.fr>
2020-09-16 15:52:01 +0200

    form control Ok

--- src/app/modules/form/components/form-ctrl/form-ctrl.component.ts
+++ src/app/modules/form/components/form-ctrl/form-ctrl.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+//Import de FormGroup et FormControl
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form-ctrl',
@@ -12,4 +14,30 @@ export class FormCtrlComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //Création d'un premier groupe à l'aide de FormGroup et non plus FormBuilder
+  userProfileForm = new FormGroup({
+    nom : new FormControl(''),
+    prenom : new FormControl('')
+  });
+
+  //Création d'un second groupe qui va récupérer les données entrées dans le premier
+  newUserProfileForm = new FormGroup({
+    nom : new FormControl(''),
+    prenom : new FormControl('')
+  });
+
+  onSubmitFirst() {
+    //Méthode magique (SetValue)
+    //this.newUserProfileForm.setValue(this.userProfileForm.value);
+
+    this.newUserProfileForm.patchValue({
+      //Deux méthodes ici pour récupérer un champ spécifique
+      nom : this.userProfileForm.controls['nom'].value,
+      prenom : this.userProfileForm.get('prenom').value
+    })
+  }
+
+  onSubmitSecond() {
+    console.log(this.userProfileForm.value);
+  }
 }
